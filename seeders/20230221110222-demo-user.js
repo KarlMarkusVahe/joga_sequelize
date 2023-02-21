e564ad5c19d859fb07dc7e8f2b43b29c5843c471
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-02-21 13:21:30 +0200

    Created article table and added table data

---
+++ seeders/20230221110222-demo-user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const {Sequelize} = require("sequelize");
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return Promise.all([
+        queryInterface.bulkInsert('Articles', [{
+          name: 'Intro to Ashtanga',
+          slug: 'intro-to-ashtanga',
+          image: 'ashtanga.jpg',
+          body: '<p>Lorem ipsum dolor sit amet</p>',
+          published: '2020-01-08 15:02:30',
+          createdAT: new Date(),
+          updatedAt: new Date()
+        }]),
+        queryInterface.bulkInsert('Articles', [{
+          name: 'Morning vinyasa routine',
+          slug: 'morning-vinyasa-routine',
+          image: 'morning.jpg',
+          body: '<p>Lorem ipsum dolor sit amet</p>',
+          published: '2020-04-14 15:02:41',
+          createdAT: new Date(),
+          updatedAt: new Date()
+        }]),
+        queryInterface.bulkInsert('Articles', [{
+          name: 'Secrets of yoga teaching',
+          slug: 'secrets-of-yoga-teaching',
+          image: 'yoga-teacher.jpg',
+          body: '<p>Lorem ipsum dolor sit amet</p>',
+          published: '2060-05-28 15:02:55',
+          createdAT: new Date(),
+          updatedAt: new Date()
+        }])
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Articles', null, {})
+  }
+};
